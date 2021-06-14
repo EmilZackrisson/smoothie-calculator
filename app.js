a1b4709ec2e7893944af1391948f812482c83a6b
Emil Zackrisson <emil.zackrisson@gmail.com>
2021-06-14 19:30:10 +0200

    första

---
+++ app.js
@@ -0,0 +1,9 @@
+function valt(){
+    var bas = document.getElementById("bas_opt").value;
+    var bland = document.getElementById("bland_opt").value;
+    var extra = document.getElementById("extra_opt").value;
+
+    document.getElementById("chosen").innerHTML = bas + bland + extra;
+
+}
+
